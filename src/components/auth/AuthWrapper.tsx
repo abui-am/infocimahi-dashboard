c5dff87bf1eb4391b2b47cb3e97e658ff1e3f53a
Abui <adjiem31@gmail.com>
2023-03-11 16:48:47 +0700

    feat: fix auth wrapper

--- src/components/auth/AuthWrapper.tsx
+++ src/components/auth/AuthWrapper.tsx
@@ -1,14 +1,25 @@
+import { useRouter } from 'next/router';
 import { signIn, useSession } from 'next-auth/react';
 import { type PropsWithChildren, useEffect } from 'react';
 
+import { api } from '@/utils/api';
+
 const AuthShowcase: React.FC<PropsWithChildren> = ({ children }) => {
   const { data: sessionData, status } = useSession();
-
+  const router = useRouter();
   useEffect(() => {
     if (status === 'unauthenticated') {
       void signIn();
     }
   }, [sessionData]);
+
+  const { data: myData, isFetched } = api.user.me.useQuery();
+
+  useEffect(() => {
+    if (!myData?.name && isFetched) {
+      void router.push(`/onboarding/${myData?.id ?? ''}`);
+    }
+  }, [myData?.name]);
 
   if (!sessionData) {
     return <div>redirecting login...</div>;
