0d91b9a6fe3191b4df585d3ec9ae880193a6a411
Abui <adjiem31@gmail.com>
2023-03-25 22:23:25 +0700

    delete example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from '@/server/api/routers/example';
 import { createTRPCRouter } from '@/server/api/trpc';
 
 import { organizationRouter } from './routers/organization';
@@ -11,7 +10,6 @@ import { userRouter } from './routers/user';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   organization: organizationRouter,
   user: userRouter,
   roles: rolesRouter,
